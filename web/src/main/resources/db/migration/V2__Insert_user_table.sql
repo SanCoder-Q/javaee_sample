e0e20dab5db8f842423298ac2f50516b7b740bf7
SanCoder <sancoder.q@gmail.com>
2015-07-09 10:31:47 +0800

    split sql script file of flyway

---
+++ web/src/main/resources/db/migration/V2__Insert_user_table.sql
@@ -0,0 +1,7 @@
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES (1,'赵明',1,'zhaoming@tw.com',20),(2,'小黄',0,'xiaohuang@tw.com',18),(3,'测试',1,'test@tw.com',17),(4,'测试2',0,'test2@gmail.com',19),(5,'测试3',1,'test3@tw.com',20),(6,'测试4',0,'test4@tw.com',21),(7,'测试4',0,'test4@tw.com',21),(8,'测试5',1,'test5@gmail.com',11),(11,'测试13',0,'test@163.com',5);
+UNLOCK TABLES;
